68c5c7d1c7075334cf717a008b3aa8a64575ea2f
millatur <minnatur7373@gmail.com>
2024-02-09 09:14:40 +0600

    edit admin clients sellers tables

--- database/migrations/2024_02_09_003823_create_clients_table.php
+++ database/migrations/2024_02_09_003823_create_clients_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->string('username')->unique()->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('password')->nullable();
+            $table->string('picture')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->enum('status',['Pending', 'Active'])->default('Pending');
             $table->timestamps();
         });
     }
