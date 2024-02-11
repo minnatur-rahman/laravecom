0154aa3ac8fb8a6b056b944982849e387034515d
millatur <minnatur7373@gmail.com>
2024-02-11 22:53:01 +0600

    edit error today

--- routes/admin.php
+++ routes/admin.php
@@ -4,14 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
 
-Route::prefix('admin')->name('admin')->group(function(){
+Route::prefix('admin')->name('admin.')->group(function(){
 
     Route::middleware(['guest:admin'])->group(function(){
         Route::view('/login','back.pages.admin.auth.login')->name('login');
     });
 
     Route::middleware(['auth:admin'])->group(function(){
-        Route::view('/home','back.pages.admin.home')->name('admin');
+        Route::view('/home','back.pages.admin.home')->name('home');
     });
 
 });
